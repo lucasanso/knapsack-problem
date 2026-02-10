d26ae2a21631369cb8770e06fbc32eb319b66ab3
Alex Benjamim de Oliveira Martins <155112179+alexoliveiramartins@users.noreply.github.com>
2026-02-10 10:57:22 -0300

    add geradorEntrada.py

---
+++ graficos/geradorEntrada.py
@@ -0,0 +1,18 @@
+import random
+import sys
+
+capacidade = 50
+n = int(sys.argv[1])
+
+pesos = [random.randint(1, 30) for _ in range(n)]
+valores = [random.randint(1, 100) for _ in range(n)]
+
+with open("entrada.txt", "w") as f:
+    f.write(f"{capacidade}\n")
+    f.write(f"{n}\n")
+    f.write(" ".join(map(str, pesos)) + "\n")
+    f.write(" ".join(map(str, valores)) + "\n")
+
+print(f"gerado arquivo entrada.txt n={n}")
+
+# python3 geradorEntrada.py n
