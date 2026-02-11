7594e2eda14e92b4ea6a46420004e4301c414c75
Alex Martins <alex.oliveiramartins05@gmail.com>
2026-02-11 16:30:47 -0300

    Implementa o codigo usando memo em c

--- codigos/01-Knapsack.c
+++ codigos/01-Knapsack.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 #include <time.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAXN 100
+#define MAXW 1000
+
+int memo[MAXN][MAXW];
 
 int max(int a, int b){
     return a >= b ? a : b;
@@ -20,13 +27,34 @@ int mochila_brute_force(int *pesos, int *valores, int capacidade, int n){
     }
 }
 
+int mochila_memo(int *valores, int *pesos, int n, int capacidade, int memo[][MAXW]){
+    if(capacidade == 0 || n == 0) 
+        return 0;
+
+    if(memo[n][capacidade] != -1){
+        return memo[n][capacidade];
+    } else {
+        int pegar = pesos[n-1] <= capacidade ? 
+            valores[n-1] + mochila_memo(valores, pesos, n-1, capacidade-pesos[n-1], memo) 
+            : 0;
+        int nao_pegar = mochila_memo(valores, pesos, n-1, capacidade, memo);
+
+        memo[n][capacidade] = max(pegar, nao_pegar);
+        return memo[n][capacidade];
+    }
+}
+
+
 int main(){
+    memset(memo, -1, sizeof(memo));
     int CAPACIDADE;
-    scanf("%d", &CAPACIDADE);
     int n;
+    scanf("%d", &CAPACIDADE);
     scanf("%d", &n);
     int pesos[n];
     int valores[n];
+
+    // int **memo[n][capacidade] = malloc((int **)sizeof(int) * n * W)
     
     for(int i = 0; i < n; i++)
         scanf("%d", &pesos[i]);
@@ -34,8 +62,9 @@ int main(){
     for(int i = 0; i < n; i++)
         scanf("%d", &valores[i]);
 
-    int maior_valor_brute =  mochila_brute_force(pesos, valores, CAPACIDADE, n);
-
-    printf("maior valor na mochila de capacidade %d e %d itens (brute force): %d\n", CAPACIDADE, n, maior_valor_brute);
+    // int maior_valor_brute = mochila_brute_force(pesos, valores, CAPACIDADE, n);
+    // printf("maior valor na mochila de capacidade %d e %d itens (brute force): %d\n", CAPACIDADE, n, maior_valor_brute);
     
+    int maior_valor_memo = mochila_memo(valores, pesos, n, CAPACIDADE, memo);
+    printf("maior valor na mochila de capacidade %d e %d itens (memo): %d\n", CAPACIDADE, n, maior_valor_memo);
 }
